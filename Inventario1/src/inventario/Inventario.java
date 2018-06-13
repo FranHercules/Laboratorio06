5046f402c7793fa33ffd057e973522eb9651b153
LN710Q <LN710Q@DEILABO1-PC32>
2018-06-13 12:40:17 -0600

    Inventario

---
+++ Inventario1/src/inventario/Inventario.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inventario;
+
+import dao.FiltroDao;
+import modelo.Filtro;
+
+/**
+ *
+ * @author LN710Q
+ */
+public class Inventario {
+    
+    public static void main (String [] args){
+        
+        FiltroDao filtro = new FiltroDao();
+        
+        filtro.create(new Filtro("hola", "hola", 4, true));
+
+    }
+    
+}
